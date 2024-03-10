b459febe060bb107c36e73c134445f87c996899d
785putt <mr.putty02@gmail.com>
2024-03-10 16:54:58 +0700

    web can display shts except images

--- anime.py
+++ anime.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
+import re
 from anime_extractor import extract_anime_titles, extract_anime_url
 
 # anime_clean.csv
@@ -11,14 +12,10 @@ csv_file_path = 'anime_clean.csv'
 anime_titles = extract_anime_titles(csv_file_path)
 anime_urls = extract_anime_url(csv_file_path)
 
-url = ''
 
 def display_dt():
-    # request allow you to send HTTP request
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')  # main scraping
 
-
+    url = ''
     b = 6.39
 
     # Dataframe Anime list 
@@ -40,8 +37,41 @@ def display_dt():
         selected_url = anime_urls[anime_titles.index(choice)]
 
         # Scrap from corresponded url
-        # if selected_url in anime_urls:
-        #     score = soup.find()
+        if selected_url in anime_urls:
+            url += selected_url
+            st.write(url)
+        
+            # request allow you to send HTTP request
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')  # main scraping
+
+            # Elements to display under anime statistics
+            score = soup.find('span', class_="score-label").text
+
+            image = soup.find('img', class_='lazyloaded')
+            
+            
+            # ranked = soup.find('div', attrs={'class': 'spaceit_pad po-r js-statistics-info di-ib', 'data-id': "info2"}).text
+            # ranked_str = re.findall(r'#([^#\s]*)', ranked)  # Extract double-quoted string values using regular expressions
+            # rstr = ' '.join(map(str,ranked_str))
+            rank = soup.find('span', attrs={'class': 'numbers ranked'}).text
+            rank_str = [text for text in re.findall(r'[^]*(?<!Ranked)[]*', rank) if text]  # Extract double-quoted string values using regular expressions
+            rst = ' '.join(map(str,rank_str))
+
+            popularity = soup.find('span', attrs={'class': 'numbers popularity'}).text
+            popular_str = [text for text in re.findall(r'[^]*(?<!Popularity)[]*', popularity) if text]  # Extract double-quoted string values using regular expressions
+            pstr = ' '.join(map(str,popular_str))
+
+            members = soup.find('span', attrs={'class': 'numbers members'}).text
+            member_str = [text for text in re.findall(r'[^]*(?<!Members)[]*', members) if text]  # Extract double-quoted string values using regular expressions
+            mstr = ' '.join(map(str,member_str))
+
+            favorites = soup.find('div', attrs={'class': 'spaceit_pad'}).text
+            # fav = favorites.find('')
+
+            synopsis = soup.find('div', attrs={'class': 'spaceit_pad'}).text
+
+
         
         # Recommend button
         is_clic = st.button("Recommends")
@@ -59,18 +89,19 @@ def display_dt():
 
         st.header(choice)
         with st.container():
+            # st.image(image)
             # st.markdown("Scores:   {}".format(b,b))
-            st.metric("Scores: ", b)
+            st.metric("Scores: ", score)
 
-            st.metric("Ranked", 1200)
+            st.metric("Ranked", rst)
             
-            st.metric("Popularity", 5000)
+            st.metric("Popularity", pstr)
 
-            st.write("Members: ", 1)
+            st.write("Members: ", mstr)
             ''' '''
-            st.write("Favourite: ", 1)
-            
-        # st.bar_chart(chart_data, x="Rating", y=["Scores"], color=["#0000FF"] )
+            # st.write("Favourite: ", favorites)
+
+            st.text(synopsis)
     
 
     st.markdown(
