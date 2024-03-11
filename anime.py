ceda6306462316d179e9a89e7b5abed0129c908d
785putt <mr.putty02@gmail.com>
2024-03-11 15:46:20 +0700

    web working with 2023 anime dataset

--- anime.py
+++ anime.py
@@ -19,38 +19,44 @@ from anime_extractor import extract_anime_titles, extract_anime_url, extract_ani
 # anime_titles = extract_anime_titles(csv_file_path)
 # anime_urls = extract_anime_url(csv_file_path)
 
-data = pd.read_csv("anime_clean.csv", encoding="utf8")
+data = pd.read_csv("anime-dataset-2023.csv", encoding="utf8")
 # print(data.head())
 
-data = data.dropna(subset=['genres'])
-data = data.dropna(subset=['source_type'])
-data = data.dropna(subset=['studios'])
-data = data.dropna(subset=['score'])
+data = data.dropna(subset=['Genres'])
+data = data.dropna(subset=['Source'])
+data = data.dropna(subset=['Studios'])
+data = data.dropna(subset=['Score'])
+# data.isnull().sum()
+
+data = data[data['Genres'] != 'UNKNOWN']
+data = data[data['Source'] != 'UNKNOWN']
+data = data[data['Studios'] != 'UNKNOWN']
+data = data[data['Score'] != 'UNKNOWN']
 
 scaler = MinMaxScaler()
 
 le = LabelEncoder()
 
-df = data[['score','genres','studios']].copy()
+df = data[['Score','Genres','Studios']].copy()
 
-df_with_id = data[['anime_id','title','score','genres','studios']].copy()
+df_with_id = data[['anime_id','Name','Score','Genres','Studios']].copy()
 
-df['score'] = scaler.fit_transform(df[['score']])
-df['genres'] = df['genres'].str.replace('|',', ')
-df['genres'] = df['genres'].str.split(', ')
-df = df.explode('genres')
+df['Score'] = scaler.fit_transform(df[['Score']])
+df['Genres'] = df['Genres'].str.split(', ')
+df = df.explode('Genres')
 
-df_with_id['score'] = scaler.fit_transform(df_with_id[['score']])
-df_with_id['genres'] = df_with_id['genres'].str.replace('|',', ')
-df_with_id['genres'] = df_with_id['genres'].str.split(', ')
-df_with_id = df_with_id.explode('genres')
+df_with_id['Score'] = scaler.fit_transform(df_with_id[['Score']])
+df_with_id['Genres'] = df_with_id['Genres'].str.split(', ')
+df_with_id = df_with_id.explode('Genres')
 
 
-df['genres'] = le.fit_transform(df['genres'])
-df['studios'] = le.fit_transform(df['studios'])
+df['Genres'] = le.fit_transform(df['Genres'])
+df['Studios'] = le.fit_transform(df['Studios'])
 
-df_with_id['genres'] = le.fit_transform(df_with_id['genres'])
-df_with_id['studios'] = le.fit_transform(df_with_id['studios'])
+df_with_id['Genres'] = le.fit_transform(df_with_id['Genres'])
+df_with_id['Studios'] = le.fit_transform(df_with_id['Studios'])
+
+# df = pd.get_dummies(df, columns=["genres", "studios"])
 
 # print(df)
 
@@ -71,7 +77,7 @@ def display_dt():
     url = ''
     b = 6.39
 
-    rad = st.sidebar.radio("Navigation", ["Main page", "User ID page"])
+    rad = st.sidebar.radio("Navigation", ["Main page", "User ID input page"])
 
     ################## Main UI layout ##################
 
@@ -87,12 +93,12 @@ def display_dt():
             if selected_url in anime_urls:
                 # Find where new point is locate in which cluster and find distance between points from chosen point
                 anime_name = choice
-                if anime_name in df_with_id['title'].values:
-                    row = df_with_id[df_with_id['title'] == anime_name].copy().drop_duplicates(subset='anime_id')
+                if anime_name in df_with_id['Name'].values:
+                    row = df_with_id[df_with_id['Name'] == anime_name].copy().drop_duplicates(subset='anime_id')
                     
                     # print(row)
 
-                    encoded = row[['score','genres','studios']].values
+                    encoded = row[['Score','Genres','Studios']].values
                     # print(encoded)
 
                     # new_point = np.array([[row[['score','genres','studios']].values]])
@@ -104,7 +110,7 @@ def display_dt():
                     top5 = []
 
                     for index, row in cluster_recommended_data.iterrows():
-                        cluster_point = np.array([[row['score'], row['genres'], row['studios']]])
+                        cluster_point = np.array([[row['Score'], row['Genres'], row['Studios']]])
                         distance = cdist(encoded, cluster_point)
                         
                         # Add the index and distance to top5 list
@@ -132,8 +138,6 @@ def display_dt():
                     final_df = df_with_id.loc[top5_ids].copy()
                     final_df = final_df.drop_duplicates(subset='anime_id')
 
-
-                # Append image
                 
                 url += selected_url
             
@@ -171,11 +175,11 @@ def display_dt():
         st.text("")
         st.text("")
         if is_clic:
-            if anime_name in df_with_id['title'].values:
+            if anime_name in df_with_id['Name'].values:
                 st.text("Here are a few similar animes recommendations  ◕⩊◕")
 
                 # st.write(final_df)
-                st.dataframe(final_df,use_container_width=True)
+                st.dataframe(final_df,use_container_width=True, column_order=("anime_id","Name","Score"), hide_index=True)
             else:
                 st.text("")
                 st.text("")
@@ -225,8 +229,8 @@ def display_dt():
             unsafe_allow_html=True,
         )
     
-    if rad == "User ID page":
-        st.write("USER ID PAGE")
+    if rad == "User ID input page":
+        st.write("USER input PAGE")
 
 
 with st.container():
